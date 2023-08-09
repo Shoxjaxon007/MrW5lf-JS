6cd28ebc468f1bb1105b8c5c37eaf29f44c21074
Shoxjaxon007 <shakarovshoxjaxon666@gmail.com>
2023-08-09 10:07:17 +0500

    fix

---
+++ MrW5lf 2.js
@@ -0,0 +1,9 @@
+let a = prompt ("Sonni kiriting")
+if (a%2===0) {
+    console.log("Juft son");
+
+}
+
+else if (a%2===1){
+    console.log("toq son");
+}
---
+++ MrW5lf.js
@@ -0,0 +1,16 @@
+let a = +prompt ("Birinchi sonni kiriting")
+let b = +prompt ("Ikkinchi sonni kiriting")
+console.log( a + b );
+let c = ( a + b )
+
+if (c%2===0) {
+    console.log("Juft son");
+
+}
+
+else if (c%2===1){
+    console.log("toq son");
+}
+else {
+    console.log("xatolik");
+}
